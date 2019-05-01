40eeb59fe0ff3daec481a5df7c8ff93afcd9e4a3
Kamila <kmilaporto1991@gmail.com>
2019-05-01 16:23:24 -0300

    broadcast GPS

---
+++ EscutadorBroadcast/app/src/main/java/com/example/escutadorbroadcast/GPSReceiver.kt
@@ -0,0 +1,32 @@
+package com.example.escutadorbroadcast
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.location.Location
+import android.location.LocationManager
+import android.provider.ContactsContract
+import android.widget.Toast
+import android.R.bool
+import android.support.v4.content.ContextCompat.getSystemService
+import android.support.v4.content.ContextCompat.getSystemService
+
+
+
+
+
+class GPSReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        //}
+        //val manager = getSystemService(Context.LOCATION_SERVICE) as LocationManager?
+        //val isOn = manager!!.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        //if (Context.LOCATION_SERVICE.toBoolean()){
+        //    Toast.makeText(context, "GPS ligado", Toast.LENGTH_LONG)
+            Toast.makeText(context, "GPS ligado", Toast.LENGTH_LONG)
+        }else{
+            Toast.makeText(context, "nada aconteceu", Toast.LENGTH_LONG)
+        }
+
+    }
+}
---
+++ EscutadorBroadcast/app/src/main/java/com/example/escutadorbroadcast/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.escutadorbroadcast
+
+import android.content.Intent
+import android.content.IntentFilter
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var status: TextView
+    private lateinit var receiver: GPSReceiver
+    private lateinit var itf: IntentFilter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        this.status = findViewById(R.id.tvStatus)
+
+        this.receiver = GPSReceiver()
+        this.itf = IntentFilter()
+        this.itf.addAction(Intent.ACTION_USER_PRESENT)
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        registerReceiver(this.receiver, this.itf)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unregisterReceiver(this.receiver)
+    }
+}
