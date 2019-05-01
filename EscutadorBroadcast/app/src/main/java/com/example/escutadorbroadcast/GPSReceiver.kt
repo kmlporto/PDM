b0b161c6b5d163b6ea18cd89dea2030b9197581a
Kamila <kmilaporto1991@gmail.com>
2019-05-01 19:19:47 -0300

    broadcast GPS - mais uma tentativa

--- EscutadorBroadcast/app/src/main/java/com/example/escutadorbroadcast/GPSReceiver.kt
+++ EscutadorBroadcast/app/src/main/java/com/example/escutadorbroadcast/GPSReceiver.kt
@@ -10,15 +10,17 @@ import android.widget.Toast
 class GPSReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
-        var manager: LocationManager = context?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        val isGPSEnabled = manager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-        if (isGPSEnabled) {
+        //var manager: LocationManager = context?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        //val isGPSEnabled = manager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+
+        if (intent.action == LocationManager.PROVIDERS_CHANGED_ACTION) {
             Toast.makeText(context, "GPS ligado", Toast.LENGTH_LONG)
             Log.i("APP_GPS", "foi")
         } else {
             Toast.makeText(context, "nada aconteceu", Toast.LENGTH_LONG)
             Log.i("APP_GPS", "nada")
         }
+
     }
+
 }
